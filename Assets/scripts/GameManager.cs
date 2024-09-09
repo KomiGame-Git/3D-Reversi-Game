8df580e610142a776474c809f2592037c16ddbe4
KomiGame-Git <komigaming97@outlook.jp>
2024-09-09 17:24:59 +0900

    first commit

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject BlockPrefab = null;
+    private AsyncOperationHandle<GameObject> OperationHandle;
+    [SerializeField]
+    private string BLOCKPREFAB_KEY = "Block";
+    [SerializeField]
+    private Vector3 BlockGenerationStartPos = Vector3.zero;
+    private const float SLIDE_NUM = 9.5f;
+    private List<List<StoneInfo>> StoneInfos = new();
+    public StoneInfo GetStoneInfo(int column_x = 0, int row_z = 0)
+    {
+        if (StoneInfos.Count == 0) { return null; }
+        if (StoneInfos.Count - 1 >= column_x && column_x >= 0)
+        {
+            if (StoneInfos[column_x].Count >= row_z && row_z >= 0)
+            {
+                return StoneInfos[column_x][row_z];
+            }
+        }
+        return null;
+    }
+
+    public GameStatus GameStatus { get; private set; } = GameStatus.Black;
+
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        OperationHandle = Addressables.LoadAssetAsync<GameObject>(BLOCKPREFAB_KEY);
+        await OperationHandle.Task;
+        BlockPrefab = OperationHandle.Result;
+        Addressables.Release(OperationHandle);
+
+        InstantiateBlocks();
+        StoneConnecting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void InstantiateBlocks()
+    {
+        Vector3 vec = Vector3.zero;
+        for (int i = 0; i < 8; i++)
+        {
+            List<StoneInfo> infos = new();
+            for (int j = 0; j < 8; j++)
+            {
+                vec = new Vector3(BlockGenerationStartPos.x + i * SLIDE_NUM, BlockGenerationStartPos.y, BlockGenerationStartPos.z + j * SLIDE_NUM);
+                GameObject gameObject = Instantiate(BlockPrefab, vec, Quaternion.identity);
+                infos.Add(gameObject.GetComponent<StoneInfo>());
+            }
+            StoneInfos.Add(infos);
+        }
+        Debug.Log($"StoneInfos.Count={StoneInfos.Count}");
+    }
+
+    void StoneConnecting()
+    {
+        const int LEFT = -1;
+        const int RIGHT = 1;
+        const int UP = -1;
+        const int DOWN = 1;
+
+        for (int i = 0; i < StoneInfos.Count; i++)
+        {
+            for (int j = 0; j < StoneInfos[i].Count; j++)
+            {
+                StoneInfo left = GetStoneInfo(i + LEFT, j);
+                StoneInfo right = GetStoneInfo(i + RIGHT, j);
+                StoneInfo up = GetStoneInfo(i, j + UP);
+                StoneInfo down = GetStoneInfo(i, j + DOWN);
+                StoneInfo left_up = GetStoneInfo(i + LEFT, j + UP);
+                StoneInfo right_up = GetStoneInfo(i + RIGHT, j + UP);
+                StoneInfo left_down = GetStoneInfo(i + LEFT, j + DOWN);
+                StoneInfo right_down = GetStoneInfo(i + RIGHT, j + DOWN);
+                StoneInfos[i][j].SetConnect(left, right, up, down, left_up, right_up, left_down, right_down);
+            }
+        }
+
+    }
+}
+
+// ゲームの進行状態を管理
+public enum GameStatus
+{
+    Black,
+    White,
+    Change,
+}
+
+// コマの状態
+public enum StoneStatus
+{
+    None,
+    Black,
+    White,
+}
+
+// コマを設置可能か否か
+public enum PutStonePossibility
+{
+    Possible_B,
+    Possible_W,
+    Possible_BW,
+    Impossible,
+}
---
+++ Assets/scripts/StoneInfo.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class StoneInfo : MonoBehaviour
+{
+    public GameObject StonePrefab { get; private set; } = null;
+    private AsyncOperationHandle<GameObject> OperationHandle;
+    [SerializeField]
+    private string STONEPREFAB_KEY = "Stone";
+    public StoneStatus Status { get; set; } = StoneStatus.None;
+    public PutStonePossibility PutPossibility { get; set; } = PutStonePossibility.Impossible;
+
+    public StoneInfo LeftStone { get; private set; } = null;
+    public StoneInfo RightStone { get; private set; } = null;
+    public StoneInfo UpStone { get; private set; } = null;
+    public StoneInfo DownStone { get; private set; } = null;
+    public StoneInfo LeftUpStone { get; private set; } = null;
+    public StoneInfo RightUpStone { get; private set; } = null;
+    public StoneInfo LeftDownStone { get; private set; } = null;
+    public StoneInfo RightDownStone { get; private set; } = null;
+
+    public bool IsLeftConnected()
+    {
+        return UpStone != null;
+    }
+    public bool IsRightConnected()
+    {
+        return RightStone != null;
+    }
+
+    public bool IsUpConnected()
+    {
+        return UpStone != null;
+    }
+
+    public bool IsDownConnected()
+    {
+        return DownStone != null;
+    }
+
+    public bool IsLeftUpConnected()
+    {
+        return LeftUpStone != null;
+    }
+
+    public bool IsRightUpConnected()
+    {
+        return RightUpStone != null;
+    }
+
+    public bool IsLeftDownConnected()
+    {
+        return LeftDownStone != null;
+    }
+
+    public bool IsRightDownConnected()
+    {
+        return RightDownStone != null;
+    }
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        OperationHandle = Addressables.LoadAssetAsync<GameObject>(STONEPREFAB_KEY);
+        await OperationHandle.Task;
+        StonePrefab = OperationHandle.Result;
+        Addressables.Release(OperationHandle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetConnect(StoneInfo left, StoneInfo right, StoneInfo up, StoneInfo down, StoneInfo leftup, StoneInfo rightup, StoneInfo leftdown, StoneInfo rightdown)
+    {
+        if(left != null){
+            LeftStone = left;
+        }
+        if(right != null){
+            RightStone = right;
+        }
+        if(up != null){
+            UpStone = up;
+        }
+        if(down != null){
+            DownStone = down;
+        }
+        if(leftup != null){
+            LeftUpStone = leftup;
+        }
+        if(rightup != null){
+            RightUpStone = rightup;
+        }
+        if(leftdown != null){
+            LeftDownStone = leftdown;
+        }
+        if(rightdown != null){
+            RightDownStone = rightdown;
+        }
+    }
+}
